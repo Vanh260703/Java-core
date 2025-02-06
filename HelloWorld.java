2266c8d77f1e31044ebfb0ccf2f3f5a44f011c54
Viet Anh <vanh260703@gmail.com>
2025-02-06 22:16:17 +0700

    init-project

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package vietanh;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world with Java");
+		System.out.println("Hello world with Java");
+		System.out.println("Hello world with Java");
+		System.out.println("Hello world with Java");
+	}
+}
