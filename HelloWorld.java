33cc226fc7d80b7395919d39587f669b8c1c3ab2
Viet Anh <vanh260703@gmail.com>
2025-02-07 23:54:44 +0700

    initial project

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,16 @@
 package vietanh;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world with Java");
-		System.out.println("Hello world with Java");
-		System.out.println("Hello world with Java");
-		System.out.println("Hello world with Java");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("First number: ");
+		int a = scanner.nextInt();
+		System.out.println("Second number: ");
+		int b = scanner.nextInt();
+		int c = Math.min(a, b);
+		System.out.println("Max number: " + c);
+		scanner.close();
 	}
 }
